ea2ca1062944ef80b1e3640dcec504af13244bf2
MADALA LEELA VENKATA SIVA SPOORTHI <mmadalavenkatesh@gmail.com>
2025-06-10 23:20:22 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,7 @@
 const Hero = () => {
   return (
     <div className="hero">
-      <img style={{
-    width: '200px',
-    height: '200px',
-    border: '2px solid #00ffcc',
-    borderRadius: '50%',
-    objectFit: 'cover',
-    display: 'block',
-    margin: '0 auto 20px'
-  }} src="src/components/meme.jpg" alt="profile" />
+      
 
       <h1>Madala Spoorthi</h1>
       
